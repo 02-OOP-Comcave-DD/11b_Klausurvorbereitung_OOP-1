2f33a62c84f3a4e10d43de619d78ec4682e081aa
Rene <media-r.r@online.de>
2024-06-03 10:40:01 +0200

    Erläuterungen erweitert

--- com/cc/java/Test.java
+++ com/cc/java/Test.java
@@ -1,14 +1,19 @@
 package com.cc.java;
 
 public class Test {
-    
+ 
+	
+
+
+
+
  void showAddress(){
     System.out.println("Instanzvariable: " + this);
 }
 
-    	// this --> Instanzvariable
+    	// this --> Instanzvariable - verweist auf sich selbst
+		// this --> ist gebunden an sich selbst
 		// verweist aus der Instanz auf sich selbst
    
-
 
 }
